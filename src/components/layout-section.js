56794528aaa6a332029dead0d04f40591a61a1df
김정환 <iz4blue@users.noreply.github.com>
2019-12-29 18:11:39 +0900

    root index 에서 표시하는 메뉴를 별도의 파일로 빼내다

--- src/components/layout-section.js
+++ src/components/layout-section.js
@@ -1,14 +1,7 @@
 import React from "react"
-import SmallWide from "./item/small-wide";
 
 export default ({ children }) => (
     <section>
       {children}
-        <div>
-          <SmallWide categoryText="Hot Coffees"/>
-          <SmallWide categoryText="Hot Teas"/>
-          <SmallWide categoryText="Hot Drinks"/>
-          <SmallWide categoryText="Frappuccino® Blended Beverages"/>
-        </div>
     </section>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "components/layout-header"
 import SubNav from "components/layout-sub_nav"
 import Section from "components/layout-section"
+import IndexRoot from "components/layout-root-index"
 import Footer from "components/layout-footer"
 import "components/global/global.css"
 
