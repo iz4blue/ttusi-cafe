4b823b6c2ee4cf94104bc359b71f3c015edb2d9e
김정환 <iz4blue@users.noreply.github.com>
2019-12-29 18:12:09 +0900

    markdown 내용에서 필드를 출력할 수 있도록 기능을 추가하다.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,27 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "components/layout-section"
-export default () => {
+export default ({ data }) => {
+  const post = data.markdownRemark
   return (
     <Layout>
-      <div>Hello blog post</div>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <h3>{post.frontmatter.category}</h3>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        category
+      }
+    }
+  }
+`
